33a344a49fbb814531df44a2561839a5bb196cda
GongPA <david.chen@spsmail.cuny.edu>
2014-11-04 23:37:07 -0500

    Update task_01.py

--- task_01.py
+++ task_01.py
@@ -9,3 +9,6 @@ def simple_lookup(var1, var2):
     except IndexError:
         print "Warning: Your index/key doesn't exist."
         return var1
+    except KeyError:
+        print "Warning: Your index/key doesn't exist."
+        return var1
